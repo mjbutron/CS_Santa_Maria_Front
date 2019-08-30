1db8cadbf1894abab29969126377ba6d9808dac1
mjbutron <butron87@gmail.com>
2019-08-30 19:17:33 +0200

    Añadido footer, logo icono blanco
    Co-Authored-By: null <mjbutron@users.noreply.github.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { HomeComponent } from './components/home/home.component';
 import { ServicesComponent } from './components/services/services.component';
 import { LoadingComponent } from './components/shared/loading/loading.component';
 import { LoginComponent } from './components/login/login.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,8 @@ import { LoginComponent } from './components/login/login.component';
     HomeComponent,
     ServicesComponent,
     LoadingComponent,
-    LoginComponent
+    LoginComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
