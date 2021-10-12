415539a7d6e2c02c484d0e001a491d3b8becbeb5
mjbutron <butron87@gmail.com>
2021-10-12 20:11:29 +0200

    Diseño detalle de servicio

--- src/app/components/servicedetails/servicedetails.component.ts
+++ src/app/components/servicedetails/servicedetails.component.ts
@@ -26,9 +26,8 @@ export class ServicedetailsComponent implements OnInit {
     this.activatedRoute.params.subscribe( param => {
       this.dataApi.getServiceById(param['id']).subscribe((data) =>{
         if (globalsConstants.K_COD_OK == data.cod){
-          this.service = data.service;
+          this.service = data.service[0];
           this.isLoaded = true;
-          console.log(this.service[0].title);
         }
         else{
           this.isLoaded = true;
