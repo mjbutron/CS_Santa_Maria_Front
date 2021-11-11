8c5e8d32be5e7ac36a1a74e3a9c688cb3da1ff32
mjbutron <butron87@gmail.com>
2021-11-11 20:32:15 +0100

    Comentarios, estructuración y eliminación de código

--- src/app/components/brands/brands.component.ts
+++ src/app/components/brands/brands.component.ts
@@ -7,63 +7,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BrandsComponent implements OnInit {
 
-  // Future: Funcionalidad para gestionar esto desde backend
+  /**
+   * [Picture list]
+   */
   slides = [
-    {img: "../../assets/img/asisa.png"},
-    {img: "../../assets/img/allianz.png"},
-    {img: "../../assets/img/adeslas.png"},
-    {img: "../../assets/img/zurich.png"},
-    {img: "../../assets/img/reale.png"},
-    {img: "../../assets/img/aegon.png"},
-    {img: "../../assets/img/liberty.png"},
-    {img: "../../assets/img/mapfre.png"},
-    {img: "../../assets/img/sanitas.png"}
+    { img: "../../assets/img/asisa.png" },
+    { img: "../../assets/img/allianz.png" },
+    { img: "../../assets/img/adeslas.png" },
+    { img: "../../assets/img/zurich.png" },
+    { img: "../../assets/img/reale.png" },
+    { img: "../../assets/img/aegon.png" },
+    { img: "../../assets/img/liberty.png" },
+    { img: "../../assets/img/mapfre.png" },
+    { img: "../../assets/img/sanitas.png" }
   ];
 
+  /**
+   * [Carousel configuration]
+   */
   slideConfig = {
-   "slidesToShow": 2,
-   "slidesToScroll": 1,
-   "centerMode": true,
-   "autoplay": true,
-   "autoplaySpeed": 5000,
-   "arrows": false,
-   "dots":true,
-   "infinite": true,
-   "pauseOnHover": false,
-   "variableWidth": true,
-   "responsive": [{
-            "breakpoint": 768,
-            "settings": {
-              "slidesToShow": 1,
-              "slidesToScroll": 1
-            }
-        }, {
-            "breakpoint": 520,
-            "settings": {
-                "slidesToShow": 1,
-                "slidesToScroll": 1
-            }
-        }]
+    "slidesToShow": 2,
+    "slidesToScroll": 1,
+    "centerMode": true,
+    "autoplay": true,
+    "autoplaySpeed": 5000,
+    "arrows": false,
+    "dots": true,
+    "infinite": true,
+    "pauseOnHover": false,
+    "variableWidth": true,
+    "responsive": [{
+      "breakpoint": 768,
+      "settings": {
+        "slidesToShow": 1,
+        "slidesToScroll": 1
+      }
+    }, {
+      "breakpoint": 520,
+      "settings": {
+        "slidesToShow": 1,
+        "slidesToScroll": 1
+      }
+    }]
   };
 
-  constructor() {
-  }
+  /**
+   * [constructor]
+   */
+  constructor() {}
 
-  ngOnInit() {}
-
-  slickInit(e) {
-   // console.log('slick initialized');
-  }
-
-  breakpoint(e) {
-   // console.log('breakpoint');
-  }
-
-  afterChange(e) {
-   // console.log('afterChange');
-  }
-
-  beforeChange(e) {
-   // console.log('beforeChange');
-  }
+  /**
+   * [ngOnInit]
+   */
+  ngOnInit() { }
 }
