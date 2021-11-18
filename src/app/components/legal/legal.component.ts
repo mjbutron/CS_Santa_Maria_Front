fcc7feaf392f32a0976bcd009ec23a1f4023567d
mjbutron <butron87@gmail.com>
2021-11-18 21:13:52 +0100

    Comentarios, estructuración y constantes en aviso legal

--- src/app/components/legal/legal.component.ts
+++ src/app/components/legal/legal.component.ts
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LegalComponent implements OnInit {
 
+  /**
+   * constructor
+   */
   constructor() { }
 
-  ngOnInit() {
-  }
-
+  /**
+   * ngOnInit
+   */
+  ngOnInit() { }
 }
