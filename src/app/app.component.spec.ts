695f12cc29a484ae9cf070ce284f55b2d1ed3a2f
mjbutron <butron87@gmail.com>
2022-01-04 11:36:09 +0100

    Pruebas unitarias en app

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,34 @@
 import { TestBed, async } from '@angular/core/testing';
+import { Component } from '@angular/core';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        MockNavbarComponent,
+        MockRouterOutletComponent,
+        MockFooterComponent
       ],
     }).compileComponents();
   }));
+
+  const app = new AppComponent();
+
+  it('Create an instance', () => {
+    expect(app).toBeTruthy();
+  });
+
 });
+
+@Component({ selector: 'app-navbar', template: '' })
+class MockNavbarComponent { }
+
+@Component({selector: 'router-outlet', template: ''})
+class MockRouterOutletComponent {
+}
+
+@Component({selector: 'app-footer', template: ''})
+class MockFooterComponent {
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
 
   /**
    * [constructor]
    */
   constructor() { }
-
-  /**
-   * [ngOnInit]
-   */
-  ngOnInit() { }
 
 }
