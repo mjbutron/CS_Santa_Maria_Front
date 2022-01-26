393d1e93943a7920201d1e715de3c556b4f06382
mjbutron <butron87@gmail.com>
2022-01-26 19:58:21 +0100

    Variables de entorno de producción

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,8 @@
 export const environment = {
   production: true,
-  pathImage: "../../cssm/assets/img"
+  urlBackEnd: "http://localhost:4300",
+  pathImage: "../../cssm/assets/img",
+  urlApiRest: "http://sisloc.es/rest",
+  imageRootPath: "http://sisloc.es/uploads/",
+  pathServerImage: "http://sisloc.es/uploads/"
 };
